61365860df2f1d4a63cc5f8d10467cb01b60c560
ash160 <89126819+kumar2021ashish@users.noreply.github.com>
2025-10-05 13:10:59 +0530

    heap

---
+++ src/HeapImplementation/MaxAndMinHeap.java
@@ -0,0 +1,8 @@
+package HeapImplementation;
+
+public class MaxAndMinHeap {
+    public static void main(String[] args){
+        System.out.println("Max and Min Heap Implementation");
+        
+    }
+}
